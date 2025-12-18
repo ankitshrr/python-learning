c65895e57d3b5a5ba967e452406c131874835110
ankitshrr <ankitshrestha677@gmail.com>
2025-12-18 15:57:54 +0545

    ''

---
+++ typecasting.py
@@ -0,0 +1,13 @@
+#Typecasting is process of changing the datatype from one datatype  to another
+#str() ,int(),float(),bool()
+name = "ankitshrr"
+age = "22"
+gpa = 3.2
+is_student = True
+ 
+gpa = int(gpa)
+print(type(gpa))
+
+
+print(str(age))
+print(type(age))
