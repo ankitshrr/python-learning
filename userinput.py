669cd7662e710dc3f0b404b6f50bc8195f30cad0
ankitshrr <ankitshrestha677@gmail.com>
2025-12-19 23:56:15 +0545

    day 2

--- userinput.py
+++ userinput.py
@@ -1,5 +1,5 @@
 #exercise
-length = input("enter the length")
-width = input("enter the width")
+length = int(input("enter the length"))
+width = int(input("enter the width"))
 area =length * width
 print(area)
