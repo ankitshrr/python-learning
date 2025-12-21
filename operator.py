60675c7f3ea6bb5548ef22ab237b08f9d331eaff
ankitshrr <ankitshrestha677@gmail.com>
2025-12-21 23:40:40 +0545

    Day 3

--- operator.py
+++ operator.py
@@ -1,8 +1,11 @@
-freind = 0
-freind = freind + 1;
+# 1. Create the variable first
+freind = 0 
 
+# 2. Now you can update it
+freind =freind + 1
 
-
-
-
-print(freind)  
+print(freind)
+# 3. shorcut waay to use it
+friends = 0
+friends +=0 
+print(friends)
