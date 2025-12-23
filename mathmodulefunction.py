1390db049ec2958d291823f9f1687e6d971edd13
ankitshrr <ankitshrestha677@gmail.com>
2025-12-23 22:24:53 +0545

    day 5

---
+++ mathmodulefunction.py
@@ -0,0 +1,17 @@
+# Math module function is a collection of math function and constants
+#Module = entire calculator
+import math 
+x = 9.5
+# print(math.pi)
+# print(math.e)
+# result=(math.sqrt(x))
+
+
+ # Ceil:round up to the  nearest whole number
+result=(math.ceil(x))
+
+#Floor:round down the the nearest integer
+# result= math.floor(x)
+
+print(result)
+
