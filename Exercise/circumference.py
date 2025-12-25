ffcf9a1e476aa87fbbee80f6773f81952da12e09
ankitshrr <ankitshrestha677@gmail.com>
2025-12-25 23:11:49 +0545

    day 6

--- Exercise/circumference.py
+++ Exercise/circumference.py
@@ -1,4 +1,4 @@
 import  math
 radius = float(input("enter the radius of the circle:"))
-circumference =math.ceil( 2 *  math.pi * radius)
-print(f"{circumference}cm")
+circumference =round( 2 *  math.pi * radius)
+print(f"the circumference is{circumference}cm")
