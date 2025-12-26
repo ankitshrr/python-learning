6f911cdee4f2918025c917a431cec9f75f83e916
ankitshrr <ankitshrestha677@gmail.com>
2025-12-26 23:19:25 +0545

    day 7

--- Exercise/hypotenuse.py
+++ Exercise/hypotenuse.py
@@ -1,7 +1,7 @@
 import math
 
-a = 3   # base
-b = 4   # height
+a = float(input("Enter base: "))
+b = float(input("Enter height: "))
 
 hypotenuse = math.sqrt(a**2 + b**2)
-print(hypotenuse)
+print("Hypotenuse =", hypotenuse)
