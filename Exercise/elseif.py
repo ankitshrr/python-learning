034eb0ca163b1d302a73371cc40a6bd4e4fe3ff1
ankitshrr <ankitshrestha677@gmail.com>
2025-12-29 13:27:36 +0545

    update

--- Exercise/elseif.py
+++
@@ -1,9 +0,0 @@
-age = input("enter the age")
-if age > 5:
-    print("age is greater than 5")
-elif age == 5:
-    print("age is equal to 5")
-elif age >= 100:
-    print("you are old")
-else:
-    print("age is less than 5")
