f8929d8511eccf833fffc08dbecf45497f882cfd
ankitshrr <ankitshrestha677@gmail.com>
2026-01-02 23:21:52 +0545

    day 7

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 6. Math Function
 7. Math Module Function
 8. If Statement
-9.  
+9.  Logical operator
 
---
+++ logicaloperator.py
@@ -0,0 +1,6 @@
+a=20
+is_sunny = False
+if a>20 or is_sunny:
+    print("it is sunny")
+else :
+    print("its not sunny")
