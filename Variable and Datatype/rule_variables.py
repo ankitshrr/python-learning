5ddc194f28020cfaaa3412df38f6e1d22b4bb334
ankitshrr <ankitshrestha677@gmail.com>
2026-01-03 18:20:00 +0545

    updated

---
+++ Variable and Datatype/rule_variables.py
@@ -0,0 +1,10 @@
+a  = 12
+b =21
+ankit =22
+sam = 2223
+_samir = 34 
+print(_samir)
+a12 = "Akit"
+print(a12)
+#we can use spiecal character to decleare variable 
+#we can use  the number to declarre variable at first bbut we can use it after adding letteer
---
+++ Variable and Datatype/variable_datatypes.py
@@ -0,0 +1,29 @@
+# Variable = a container for a value(string,boolean,float,integer)used to hold or store the data
+
+# string
+first_name = "hello"
+food = "banana"
+print(first_name)
+print(f"{first_name} Ankit")
+print(f"i like {food} fruit")
+
+# integer
+age = 25
+quantity = 4
+phone_num = 2312
+print(f"you are {age} years old")
+print(f"you are buying {quantity} items")
+print(f"your number is {phone_num}")
+
+#float 
+price = 7.23
+distance =5.3
+print(f"the price is {price}")
+print(f"you distance is {distance}km")
+
+#boolean
+is_student= True
+
+print(f"you are a student?:{is_student}")
+
+e = None #e is a non type variable
