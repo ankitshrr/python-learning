d4b85486806e10ef92762bdd762e1c9129109bf3
ankitshrr <ankitshrestha677@gmail.com>
2026-01-03 18:17:54 +0545

    updated

---
+++ Statement/elseif.py
@@ -0,0 +1,9 @@
+age = input("enter the age")
+if age > 5:
+    print("age is greater than 5")
+elif age == 5:
+    print("age is equal to 5")
+elif age >= 101:
+    print("you are old")
+else:
+    print("age is less than 5")
