d11020887260b367c6958ac37579692898ecba35
ankitshrr <ankitshrestha677@gmail.com>
2026-01-03 13:38:52 +0545

    finished

---
+++ Exercise/Practice/problem1.py
@@ -0,0 +1,20 @@
+#print multiline using only one statement
+print('''sds
+      sdddsdsda
+      a
+      ds
+      dsds
+      d
+      
+      d
+      ds
+      ds
+      d
+      s
+      ds
+      d
+      sad
+      s
+      adsa
+      d
+      ssadas''')
