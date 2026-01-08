186c542aa78d3cd83050b379a03a9a78972f9901
ankitshrr <ankitshrestha677@gmail.com>
2026-01-08 15:07:55 +0545

    add

---
+++ Chapter - 4 List and Tuple/Practice/problem3.py
@@ -0,0 +1,4 @@
+chek = (121,32,232232)
+result = str(chek)
+print(type(result))
+print(type(chek[0]))
---
+++ Chapter - 4 List and Tuple/Practice/problem4.py
@@ -0,0 +1,3 @@
+number = [2,3,4,5]
+result =sum(number)
+print(result)                                           
