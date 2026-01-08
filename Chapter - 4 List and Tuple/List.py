5562b011aef6cfc45926d88514128aecb3e68ff8
ankitshrr <ankitshrestha677@gmail.com>
2026-01-08 12:30:33 +0545

    add

--- Chapter - 4 List and Tuple/List.py
+++ Chapter - 4 List and Tuple/List.py
@@ -2,5 +2,5 @@ club =["barca","real madrid", "juv"]# LIST ARE MUTABLE
 club[0]= 'fuck u barca'
 print(club)
 print(club[2])
-club.append(23)
+
 print(club)
---
+++ Chapter - 4 List and Tuple/list_method.py
@@ -0,0 +1,25 @@
+club=['barca','realmadrid','ankit']
+
+club.append(23)
+print(club)
+li = ["wanzy","wanzY","Wanzy","wanzy"]# sort the list word letter by letter  using asci value 
+li.sort() #sort the number and string in squeneely by lower to higher
+li.reverse()
+print(li)
+
+
+
+num =[1,2,4]
+# insert(,index,element)
+num.insert(2,"ankit")
+re = num.pop(3)
+#print(num.pop(3))
+
+num.remove("ankit")
+num.extend(li)
+print(num)
+
+
+
+counts= ["wanzy","wanzy"]
+print(counts.index("wanzy"))
