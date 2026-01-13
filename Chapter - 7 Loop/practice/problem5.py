72cdfe17c1dc89ac3a0fd4e78de9e05af9a69b00
ankitshrr <ankitshrestha677@gmail.com>
2026-01-13 23:47:51 +0545

    add

--- Chapter - 7 Loop/practice/problem5.py
+++ Chapter - 7 Loop/practice/problem5.py
@@ -1,6 +1,14 @@
-n = int(input("enter the sum of first n "))
-i = 1
-while i <= n:
+# n = int(input("enter the sum of first n "))
+# i = 1
+# while i <= n:
 
-    print(round(i * (i + 1) / 2) )
+#     print(round(i * (i + 1) / 2) )
+#     i += 1
+n = int(input("enter nuber of first sum"))
+i =1 
+sum = 0
+while(i<=n):
+    sum += i
     i += 1
+    print(i)
+
