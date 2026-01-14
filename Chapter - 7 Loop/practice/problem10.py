d44eea864ac42a0ca506404ca87a645f02540c99
ankitshrr <ankitshrestha677@gmail.com>
2026-01-14 21:46:42 +0545

    update

---
+++ Chapter - 7 Loop/practice/problem10.py
@@ -0,0 +1,3 @@
+n = int(input("enter a number to  find the reverse multiplication:"))
+for i in range(10,0,-1):
+ print(f"{n} x {i} = {n * i }")
---
+++ Chapter - 7 Loop/practice/problem8.py
@@ -0,0 +1,5 @@
+rows = 3 
+for i in range(0,3):
+    for  j in range(0,i + 1):
+        print("*",end="")
+    print()
