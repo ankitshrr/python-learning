0f8057d2f538f3c2702f646d97e5a9c143d3f895
ankitshrr <ankitshrestha677@gmail.com>
2026-01-14 12:54:21 +0545

    add

---
+++ Chapter - 7 Loop/practice/prblem6.py
@@ -0,0 +1,17 @@
+n = int(input("enter factorial"))
+fact = 1
+for i in range(1,n+1):
+    fact= fact *  i
+    print(fact)
+
+# n = int(input("enter factorial"))
+# i=1
+# fact =1
+
+# while(i<=n):
+#  fact = fact * i
+ 
+#  print(fact)
+
+ 
+ 
--- Chapter - 7 Loop/prblem6.py
+++
@@ -1,5 +0,0 @@
-n = int(input("enter factorial"))
-curr = 1
-for i in range(1,n):
-    curr = curr * 
-    p
