37153757eac1b97ae08131c3a2149d788fa7322b
ankitshrr <ankitshrestha677@gmail.com>
2026-01-30 15:55:45 +0545

    set completed

--- chapteer 9 file/PS/problem 7/problem6.py
+++ chapteer 9 file/PS/problem 7/problem6.py
@@ -1,10 +1,19 @@
 # Write a program to find out the line number where python is present from ques 6.
-with open("doc.log") as f:
-    content = f.read().lower()
+# with open("doc.log") as f:
+#     content = f.read().lower()
     
 
-words= content.split()
+# words= content.split()
 
-for word in words:
-    if "python" == word:
-     print(f"its contain python :{word} ")
+# for word in words:
+#     if "python" == word:
+#      print(f"its contain python :{word} ")
+with open("Doc.log") as f:
+    lineno = 1
+    for line in f:
+        if "python" in line:
+            print(f"working fine! {lineno}")
+            break
+        lineno += 1
+    else:
+        print("Error")
