90c52c7ecd7088c7592f4fd046cc616935b4516a
ankitshrr <ankitshrestha677@gmail.com>
2026-02-02 17:01:25 +0545

    Learned Python Classes

---
+++ Chapter -10 OOPS/1_Class.py
@@ -0,0 +1,12 @@
+class Employee:
+    name = "harry"
+    language = " py" # this a class atrribute
+    salary = 1200000 # this a classs atttribute
+
+
+harry = Employee() 
+harry.name = "ankit"   # this a object or instance attribute  and name to ankit 
+print(harry.name,harry.language,harry.salary)
+
+
+# here name is object attribute and salary,language are  class attribute as they direclty belong to class
