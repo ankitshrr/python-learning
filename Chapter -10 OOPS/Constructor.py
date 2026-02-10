522e4ec86403e4890009163710e0c3728f430a15
ankitshrr <ankitshrestha677@gmail.com>
2026-02-10 22:56:12 +0545

    learned constructor

--- Chapter -10 OOPS/Constructor.py
+++ Chapter -10 OOPS/Constructor.py
@@ -4,8 +4,12 @@ class Employee :
     def getInfo(self):
      print(f"the language is {self.language}. THE salary is {self.salary}")
     
-    def __init__(self): #dunder method whic is autmocally cal
-       print("i am lerning ")
-ankit =Employee()
+    def __init__(self,name,salary,lanhague):#dunder method whic is autmocally call the clsss 
+       self.name=name
+       self.salary=salary
+       self.lanhague=lanhague 
+       
+ankit =Employee("harry",10000,"javascript")
+print(ankit.name)
 ankit.language= "as"
 ankit.getInfo() 
