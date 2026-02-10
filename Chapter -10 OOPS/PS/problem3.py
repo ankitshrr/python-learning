970fc1da5cfa1e539c122dca4a7c23bca4005799
ankitshrr <ankitshrestha677@gmail.com>
2026-02-10 23:56:14 +0545

    practice set completed

---
+++ Chapter -10 OOPS/PS/problem3.py
@@ -0,0 +1,9 @@
+#  Create a class with a class attribute a; create an object from it and set ‘a’ directly using ‘object.a = 0’. Does this change the class attribute? 
+class program :
+     a = "ankit"
+
+ankit = program()
+ankit.a=0 
+print(ankit.a)
+print(program.a)
+
