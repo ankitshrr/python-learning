67e4063e9f4110b03f46a5defa4aab5a3d0ff818
ankitshrr <ankitshrestha677@gmail.com>
2026-02-17 23:57:36 +0545

    Revise negativeSlicing

--- Chapter -3  String/negative_slicing.py
+++ Chapter -3  String/negative_slicing.py
@@ -1,5 +1,7 @@
 # negative slice 
 # better to avoid negative slice
 a = "ankita"#-6 -5 -4 -3 -2 -1
-# print(a[-6:7])
+print(a[-6:7])
 print(a[-7:-1]) 
+print(a[-7:-1:-2])#❌
+print(a[-7:-1:2])#Start index must be GREATER than end index
