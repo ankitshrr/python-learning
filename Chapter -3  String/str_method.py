703430cc7aca26a4d996d98731be3c2fcbe9f3a5
ankitshrr <ankitshrestha677@gmail.com>
2026-02-18 21:22:27 +0545

    Learn count,isdigit,split and strip methods

--- Chapter -3  String/str_method.py
+++ Chapter -3  String/str_method.py
@@ -1,8 +1,25 @@
-a ="vr7"
+a ="vr7 ankit"
 # print(len(a))
 # print(a.endswith("r7"))
 print(a.startswith("V"))
 print(a.upper())
 print(a.capitalize())
 
-print(a.title())
+print(a.title())#capitalize each word first letter except middlespace
+text = "  QA Engineer  "
+
+print(text.strip())
+text = "QQQA EngineerQQQ"
+print(text.strip("Q"))
+
+data = "apple,banana,orange"
+print(data.split(","))# SPLIT THE STRING INTO LIST
+
+
+num="1232323"
+print(num.isdigit()) #check numeric
+
+
+
+text = "test test test"
+print(text.count("test"))#Counts occurrences of substring.
