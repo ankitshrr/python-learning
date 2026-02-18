a3bed46d75e4ef9215c86c6cd5c95cad24291337
ankitshrr <ankitshrestha677@gmail.com>
2026-02-18 21:32:21 +0545

    Add clearer comment

--- Chapter -3  String/practice/problem1.py
+++ Chapter -3  String/practice/problem1.py
@@ -1,2 +1,3 @@
-a  = input("enter the name");
-print("good afternoon,",a)
+#Write a python program to display a user entered name followed by Good Afternoon using input () function.
+name= input("enter the name");
+print("good afternoon,",name)
