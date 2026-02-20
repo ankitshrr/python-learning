a04363412d461324393a9738c06abd8e26abb595
ankitshrr <ankitshrestha677@gmail.com>
2026-02-20 12:27:59 +0545

    Revise list methods and add comments

--- Chapter - 4 List and Tuple/list_method.py
+++ Chapter - 4 List and Tuple/list_method.py
@@ -1,27 +1,35 @@
-club=['barca','realmadrid','ankit']
+# List of football clubs (strings)
+club = ['barca', 'realmadrid', 'ankit']
 
-club.append(23)
+# append() → adds element at the end of list
+club.append(23)   # adding number into string list (mixed list allowed in Python)
 print(club)
-li = ["wanzy","wanzY","Wanzy","wanzy"]# sort the list word letter by letter  using asci value 
-li.sort() #sort the number and string in squeneely by lower to higher
-li.reverse()
+
+
+# List of words with different uppercase/lowercase
+li = ["wAnzy", "wanzY", "Wanzy", "wanzy", 'a']
+
+# sort() → sorts strings using ASCII (uppercase first, then lowercase)
+li.sort()
+# li.reverse()  # reverse() → would reverse the sorted list
 print(li)
 
 
+# Numeric list
+num = [1, 2, 4]
 
-num =[1,2,4]
-# insert(,index,element)
-num.insert(2,"ankit")
-re = num.pop(3)
-#print(num.pop(3))
+# insert(index, element) → inserts element at given index
+num.insert(2, "ankit")   # insert string at index 2
+# num becomes [1, 2, 'ankit', 4]
 
-num.remove("ankit")
-num.extend(li)
+# pop(index) → removes and returns element from given index
+re = num.pop(3)   # removes 4
 print(num)
 
 
+# remove(value) → removes element by value (not index)
+num.remove("ankit")   # removes 'ankit' from list
 
-counts= ["wanzy","wanzy",1]
-#print(counts.index(1))
-counts.count(1)
-print(counts)
+# extend(iterable) → adds all elements of another list into this list
+num.extend(li)   # adding sorted li elements into num
+print(num)
