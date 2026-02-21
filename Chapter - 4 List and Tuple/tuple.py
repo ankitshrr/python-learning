55fddc6dee48df7e6f0cef0b9fd964f31780edb5
ankitshrr <ankitshrestha677@gmail.com>
2026-02-21 22:02:50 +0545

    Revise tuple add comments

--- Chapter - 4 List and Tuple/tuple.py
+++ Chapter - 4 List and Tuple/tuple.py
@@ -1,3 +1,9 @@
-a = (1,45,432,2.55,"tupds")#tuple are immutable
+a = (1,45,432,2.55,55,"tuple")#tuple allow duplicate 
 print(a)
 print(type(a))
+
+
+
+a[0]=1 #immutable
+print(a[1]) # access tuple element(ordered)
+
