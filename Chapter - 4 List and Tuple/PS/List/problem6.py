d625e46832fd68bb304b59a024c86e9a4299ceed
ankitshrr <ankitshrestha677@gmail.com>
2026-02-25 23:43:10 +0545

    Solved list problem to find sum

--- Chapter - 4 List and Tuple/PS/List/problem6.py
+++ Chapter - 4 List and Tuple/PS/List/problem6.py
@@ -1,6 +1,16 @@
 #Create a list and find total sum
-li=[1,3,5,6]
+li=[1,3,2,3,3,5,6]
 sum = 0
 for i in li:
    sum += i
 print(sum)
+
+
+n=0
+total =0
+while n <len(li):
+   total+=li[n]
+   n+=1
+
+
+print(total)
