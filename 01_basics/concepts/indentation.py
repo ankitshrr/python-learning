ebc2c7b2daa055209f1d34e85eb2ae6137ace04b
ankitshrr <ankitshrestha677@gmail.com>
2026-02-26 20:20:24 +0545

    learn indentation concept

---
+++ 01_basics/concepts/indentation.py
@@ -0,0 +1,4 @@
+#this line demonstrate indentation rule
+if True: #condition is always true
+    print("correct indentation") # indented line belongs to if block
+    # Indented print() → part of if block
