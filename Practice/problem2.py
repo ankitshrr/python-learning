a63987fee705449aa7f9ae6124888e13371ae28f
Ankit Shrestha <ankitshrestha677@gmail.com>
2026-03-02 23:17:54 +0545

    Add second largest number without sorting solution

--- Practice/problem2.py
+++ Practice/problem2.py
@@ -1,1 +1,14 @@
-#Use ripple to print the table of 5
+#Find Second Largest Number (Without using sort())
+nums = [10, 5, 8, 20, 15]
+
+largest = nums[0]
+secondlargest = float('-inf')
+
+for num in nums:
+    if num > largest:
+        secondlargest = largest
+        largest = num
+    elif num > secondlargest and num != largest:
+        secondlargest = num
+
+print(secondlargest)
