9e8671978e598408e7f087234291b5cb54c43ba0
Ankit Shrestha <ankitshrestha677@gmail.com>
2026-03-04 23:48:14 +0545

    Learn _ Variable Concept

---
+++ Chapters/_Variable.py
@@ -0,0 +1,13 @@
+# range(5) gives numbers: 0, 1, 2, 3, 4
+# but we don't need these numbers
+# we just want to run the loop 5 times
+
+for _ in range(5):   # "_" means we are ignoring the value
+    print("Hello")
+
+
+# here we use variable i
+# but we are not using i anywhere
+
+for i in range(5):
+    print("Hello world")
