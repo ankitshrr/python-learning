6acaa43731aec71370b6fac8bfd59a9e9a49cb98
ankitshrr <ankitshrestha677@gmail.com>
2026-03-23 23:50:18 +0545

    Revise Enumerate function

--- Chapters/Enummerate.py
+++ Chapters/Enummerate.py
@@ -1,3 +1,4 @@
+# Enumerate is a built-in function in Python that allows you to loop through an iterable and have an automatic counter. It returns both the index and the value of each item in the iterable.
 names = ["Ankit", "Ram", "Shyam"]
 
 for index, name in enumerate(names):
