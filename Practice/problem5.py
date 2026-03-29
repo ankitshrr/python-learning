88cfeb7fe79c4066e2a4f7f7928f317c7cdcf2e2
ankitshrr <ankitshrestha677@gmail.com>
2026-03-29 23:46:51 +0545

    Practice Repeat string problem"

--- Practice/problem5.py
+++
@@ -1,7 +0,0 @@
-#label the program written in problem 5 with comment
-
-#find the path of folder in c drive
-import os 
-directory_path = '/'
-contnt= os.listdir(directory_path);
-print(contnt)
---
+++ Practice/repeat_string_then_add.py
@@ -0,0 +1,5 @@
+# This code will print the following:
+a = "3"
+a*=2
+print(int(a)+5) 
+print(a)
