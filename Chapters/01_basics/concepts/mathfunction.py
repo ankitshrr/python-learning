366cdb7504e1195a070168c2d6aaa67fa295e747
ankitshrr <ankitshrestha677@gmail.com>
2026-05-19 00:10:03 +0545

    Revise math functin

--- Chapters/01_basics/concepts/mathfunction.py
+++ Chapters/01_basics/concepts/mathfunction.py
@@ -1,17 +1,17 @@
 # Math functions = built-in tools used for calculations
 
 y = 3.14   # decimal number
-x = 1      # integer
+x = -1      # integer
 z = 5      # integer
 a = 1      # integer
 
 # round(number, digits)
 # Rounds a number to given decimal places
 # result = round(y, x)     # rounds 3.14 to 1 decimal → 3.1
-# result = round(y)        # rounds 3.14 to nearest whole number →
+
 # abs(number)
 # Returns positive value (removes negative sign)
-result = abs(x)            # abs(1) → 1
+# result = abs(x)            # abs(1) → 1
 
 # max(numbers)
 # Returns the largest value
@@ -23,7 +23,7 @@ result = abs(x)            # abs(1) → 1
 
 # pow(base, exponent)
 # Raises number to a power
-# result = pow(z, y)       # 5 raised to 3.14
+result = pow(z, y)       # 5 raised to 3.14
 
 print(result) 
 
