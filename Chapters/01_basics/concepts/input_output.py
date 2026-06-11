94612dd5ef363ca7c68bc747962fce64088a89b6
Ankit Shrestha <ankitshrestha677@gmail.com>
2026-06-11 23:43:08 +0545

    Revise typecasting with input functiom

--- Chapters/01_basics/concepts/input_output.py
+++ Chapters/01_basics/concepts/input_output.py
@@ -1,3 +1,4 @@
 # this program take input from user and display using print function
 name = input("enter name")#input() take the input from user
 print(name)# display the text
+lastName = float(input ("enter last name"))
