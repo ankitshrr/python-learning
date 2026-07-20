2ecb097c2697a61369e60953d79f81beef1637b7
ankitshrr <ankitshrestha677@gmail.com>
2026-07-20 23:54:25 +0545

    refactor: revise string practice code

--- Chapters/03_strings/concepts/string_slicing.py
+++ Chapters/03_strings/concepts/string_slicing.py
@@ -1,14 +1,17 @@
 # negative slice 
 # better to avoid negative slice
-a = "ankita"#-6 -5 -4 -3 -2 -1
-b ="ankitaa"
-# print(b[-6:6])
-# print(a[-7:-1]) 
-# print(a[-7:-1:-2])#❌
-# print(a[-7:-1:2])#Start index must be GREATER than end index
+football_idol = "Ronaldo"# -7 -6 -5 -4 -3 -2 -1 -0
+name ="ankit"
+print(name[-5:5])
+print(football_idol[-7:-1]) 
 
+print(football_idol[-7:-1:2])
 
-# a =  [start:end:step]
-slicing = a [1:2:3]
-print(b[0:7:2])
+#always use positive slicing 
+# name =  [start:end:step]
+slicing = football_idol [1:2:3]
 print(slicing)
+print(name[0:7:2])
+letter = "ABCDEFGHIJ"
+print(letter[-2:-1])
+
