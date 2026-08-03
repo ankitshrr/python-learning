a240fb9990a8eddbb00e199d662d2a4fcf125017
ankitshrr <ankitshrestha677@gmail.com>
2026-08-03 23:59:40 +0545

    learn: average calculation

---
+++ playground/code.py
@@ -0,0 +1,6 @@
+p1= float(input("enter the price of product 1:"))
+p2= float(input("enter the price of product 2:"))
+p3= float(input("enter the price of product 3:"))
+total =p1 + p2 + p3
+average = round(total / 3,2)
+print(average)
