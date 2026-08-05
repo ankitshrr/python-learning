2eb60ad9703681c6662258163d960fce3b7b5a45
ankitshrr <ankitshrestha677@gmail.com>
2026-08-05 21:44:38 +0545

    revise ternary operator

--- 01_basics/05_conditional_expression.py
+++ 01_basics/05_conditional_expression.py
@@ -2,4 +2,7 @@ age =15
 # conditional expression or Ternary Operator
 result = print("eligible") if age > 14  else print("not eligible")
 
-                                            
+price = 50
+ 
+number = int(input("enter a number:"))
+positiveNumber= print("positive") if (number>0) else print("negative")
